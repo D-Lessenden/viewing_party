87088f18f55d896106c31791f0aa8988b0ff7556
Judith Pillado <judith.pillado@gmail.com>
2020-10-06 16:08:17 -0500

    Add indentation per RuboCop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-has_secure_password
+  has_secure_password
 
   validates_presence_of :email, :password
+  # validates :email, presence: true
+  # validates :password, presence: true
 
   # enum role: ['default']
 end
