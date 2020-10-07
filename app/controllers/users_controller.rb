6a783bda8c0923d4fbc90b49eac4b2c20fcd9f75
Judith Pillado <judith.pillado@gmail.com>
2020-10-07 18:01:15 -0500

    Fix RuboCop violations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -12,18 +11,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Welcome #{user_params["email"]}, you are now registered and logged in!"
-      redirect_to "/dashboard"
+      flash[:success] = "Welcome #{user_params['email']}, you are now registered and logged in!"
+      redirect_to '/dashboard'
     else
       flash[:errors] = @user.errors.full_messages.to_sentence
-      if @user.errors.details.keys.include?(:email)
-        @user.email = ""
-      end
-      redirect_to "/registration"
+      @user.email = '' if @user.errors.details.keys.include?(:email)
+      redirect_to '/registration'
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
