40438857358b54b5fbfb76a57eef43c23fb0b3d8
Daniel Lessenden <dless27@gmail.com>
2020-10-07 17:44:56 -0500

    Add tests for relationships with followers and followed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe User do
     it {should validate_presence_of :username}
   end
 
+  describe "relationships" do
+    it {should have_many :following_users}
+    it {should have_many(:followers).through(:following_users)}
+    it {should have_many :followed_users}
+    it {should have_many(:followed).through(:followed_users)}
+  end
 end
