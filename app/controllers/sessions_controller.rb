f4dbe7cd6c9c36e434b161d42e728f9552666c88
Judith Pillado <judith.pillado@gmail.com>
2020-10-07 23:48:57 -0500

    Add destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,4 +10,10 @@ class SessionsController < ApplicationController
       flash[:notice] = 'Incorrect email or password'
     end
   end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'You have logged out. See you next time!'
+    redirect_to root_path
+  end
 end
