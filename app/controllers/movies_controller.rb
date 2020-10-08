f59be78d1a17347b52d7fef8b8c1dce463e8f277
Daniel Lessenden <dless27@gmail.com>
2020-10-08 13:33:22 -0500

    Delete the search action for a more restful setup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   def index
-  end
-
-  def search
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+      faraday.headers["X-API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
     end
-
   end
+
+  #x-api-key=>ENV['PROPUBLICA_API_KEY']
+
+
+
 end
