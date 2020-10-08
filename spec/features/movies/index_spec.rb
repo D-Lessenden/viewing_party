8b55677de16c7d34cee37d53cb4f7ffca7883a84
Daniel Lessenden <dless27@gmail.com>
2020-10-08 13:35:19 -0500

    Add test for movie index page

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "As an authenticated user" do
-  describe "When I visit the '/discover' path" do
-    it "should have a button to discover top 40 movies" do
-      visit '/discover'
-      expect(page).to have_button("Discover Top 40")
-      click_button "Discover Top 40"
-      expect(current_path).to eq('/movies')
+  describe "when I visit the movies page" do
+    it "has a field to search for movies" do
+      visit '/movies'
       expect(page).to have_button("Search by Movie Title")
       click_button "Search by Movie Title"
     end
