4bb1e61543708223a08a0db5884d32104675d252
Daniel Lessenden <dless27@gmail.com>
2020-10-08 23:42:09 -0500

    Delete commented out code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,6 @@ class MoviesController < ApplicationController
     next20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=2")
     @json1 = JSON.parse(top20.body, symbolize_names: true)
     @json2 = JSON.parse(next20.body, symbolize_names: true)
-    #dont know how to make this test
   end
 
   def search
@@ -18,12 +17,5 @@ class MoviesController < ApplicationController
     response = conn.get("/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=#{search}")
     json = JSON.parse(response.body, symbolize_names: true)
     @films = json[:results]
-
-
-
   end
-  #api-key= ENV['PROPUBLICA_API_KEY']
-  #config app yml
-  #figaro
-
 end
