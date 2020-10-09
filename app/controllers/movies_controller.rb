92cb117b81b0006d86f3f6235ae1423dfed0f689
Daniel Lessenden <dless27@gmail.com>
2020-10-08 21:53:40 -0500

    Add api key and responses for index and search actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,29 @@
 class MoviesController < ApplicationController
   def index
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["X-API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
     end
+    top20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=1")
+    next20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=2")
+    @json1 = JSON.parse(top20.body, symbolize_names: true)
+    @json2 = JSON.parse(next20.body, symbolize_names: true)
+    #dont know how to make this test
   end
 
-  #x-api-key=>ENV['PROPUBLICA_API_KEY']
+  def search
+    search = params[:search]
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+    end
+    response = conn.get("/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=#{search}")
+    json = JSON.parse(response.body, symbolize_names: true)
+    @films = json[:results]
 
 
+
+  end
+  #api-key= ENV['PROPUBLICA_API_KEY']
+  #config app yml
+  #figaro
 
 end
