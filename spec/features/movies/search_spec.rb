29ad10c7773252d39740e9598d035e8239e05fee
Daniel Lessenden <dless27@gmail.com>
2020-10-08 23:39:57 -0500

    Create file to test the API with a stub

---
+++ spec/features/movies/search_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "As an authenticated user" do
+  describe "when I visit the movie search page" do
+    it "has a field to search for movies" do
+
+      search_response = File.read('spec/fixtures/search_result_fight.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=fight").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'Api-Key'=>'c74a8c939b809c17fcffb22fc6e5fd03',
+         'User-Agent'=>'Faraday v1.0.1'
+          }).
+        to_return(status: 200, body: search_response)
+
+      # visit '/movieqs/search'
+       # fill_in 'search', with: "fight"
+       # click_button "Search by Movie Title"
+       # expect(current_path).to eq('/movies/search')
+    end
+  end
+end
