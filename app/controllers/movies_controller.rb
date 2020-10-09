14439d32b499c02c6f44e185eda4cb52b7d526b7
Judith Pillado <judith.pillado@gmail.com>
2020-10-09 17:54:20 -0500

    Rubo Cop Violations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['API-Key'] = 'c74a8c939b809c17fcffb22fc6e5fd03'
     end
-    top20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=1")
-    next20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=2")
+    top20 = conn.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=1')
+    next20 = conn.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c74a8c939b809c17fcffb22fc6e5fd03&page=2')
     @json1 = JSON.parse(top20.body, symbolize_names: true)
     @json2 = JSON.parse(next20.body, symbolize_names: true)
   end
 
   def search
     search = params[:search]
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['API-Key'] = 'c74a8c939b809c17fcffb22fc6e5fd03'
     end
     response = conn.get("/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=#{search}")
     json = JSON.parse(response.body, symbolize_names: true)
