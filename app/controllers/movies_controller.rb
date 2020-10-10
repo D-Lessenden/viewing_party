6e49119ad1c3a1d10415a6d973cc39f683b95d58
Daniel Lessenden <dless27@gmail.com>
2020-10-10 03:00:54 -0500

    Movie details info

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,14 +19,13 @@ class MoviesController < ApplicationController
     @films = json[:results]
   end
 
-  def show
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
-    end
-    response = conn.get("/3/movie/#{params[:id]}?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
-    cast_response = conn.get("/3/movie/#{params[:id]}/credits?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
-    @json = JSON.parse(response.body, symbolize_names: true)
-    @cast = JSON.parse(cast_response.body, symbolize_names: true)
-    binding.pry
-  end
+  # def show
+  #   conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+  #     faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
+  #   end
+  #   response = conn.get("/3/movie/#{params[:id]}?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
+  #   cast_response = conn.get("/3/movie/#{params[:id]}/credits?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
+  #   @json = JSON.parse(response.body, symbolize_names: true)
+  #   @cast = JSON.parse(cast_response.body, symbolize_names: true)
+  # end
 end
