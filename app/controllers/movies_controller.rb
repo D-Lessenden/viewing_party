0b2a59cf50e22672fbecfece1d720f62a41c57d3
Daniel Lessenden <dless27@gmail.com>
2020-10-11 12:37:11 -0500

    Add logic to show runtime and reviews in the view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
       faraday.headers["API-Key"] = 'c74a8c939b809c17fcffb22fc6e5fd03'
     end
-    response = conn.get("/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=#{search}")
+    response = conn.get("/3/search/movie?api_key=c74a8c939b809c17fcffb22fc6e5fd03&query=#{@search}")
     json = JSON.parse(response.body, symbolize_names: true)
     @films = json[:results]
   end
@@ -25,10 +25,10 @@ class MoviesController < ApplicationController
     end
     response = conn.get("/3/movie/#{params[:id]}?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
     cast_response = conn.get("/3/movie/#{params[:id]}/credits?api_key=c74a8c939b809c17fcffb22fc6e5fd03")
-    # runtime = conn.get("")
+    review_response = conn.get("/3/movie/#{params[:id]}/reviews?api_key=c74a8c939b809c17fcffb22fc6e5fd03&language=en-US")
     @json = JSON.parse(response.body, symbolize_names: true)
     @cast = JSON.parse(cast_response.body, symbolize_names: true)
-    # binding.pry
+    @reviews = JSON.parse(review_response.body, symbolize_names: true)
   end
 
 end
