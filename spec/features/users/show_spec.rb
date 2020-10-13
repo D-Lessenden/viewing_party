1d2462491231f41c6bf31ffb9a7396762b4ffb89
Daniel Lessenden <dless27@gmail.com>
2020-10-12 22:47:03 -0500

    Create test for add friend

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'user dashboard', type: :feature do
   before :each do
-    @user = User.create(
+    @user = User.create!(
                     username: 'USERNAME',
                     email: 'example@email.com',
                     password: 'Hunter2',
-                    role: 0)
+                    role: 0
+                    )
+
+    @friend = User.create!(
+                    username: 'FRIEND',
+                    email: 'friend@email.com',
+                    password: 'Hunter2',
+                    role: 0
+                    )
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
     describe 'As an authenticated user when I visit my dashboard' do
@@ -23,6 +31,16 @@ RSpec.describe 'user dashboard', type: :feature do
         click_button "Discover Movies"
         expect(current_path).to eq('/discover')
         expect(page).to have_content("Discover Your Next Favorite Movie!")
+      end
+
+      it "has a field where you can add a friend" do
+        visit '/dashboard'
+        fill_in :friend, with: 'friend@email.com'
+        click_on 'Add Friend'
+        #check to make sure table exists
+        #check sad path.
+        #add flashes to both 
+
       end
     end
 end
