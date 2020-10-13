3a56267477f3532c67fcf9472961f3426712a2b5
Daniel Lessenden <dless27@gmail.com>
2020-10-13 17:57:39 -0500

    Delete unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get '/registration', to: 'users#new'
   post '/users', to: 'users#create'
   get '/dashboard', to: 'users#show'
-  get '/users/add_friend', to: 'users#add_friend'
   post '/users/add_friend', to: 'users#add_friend'
 
   get '/movies/search', to: 'movies#search'
