f52e322eaec23af8dc35e9b29f3b8ac3abeddc89
Daniel Lessenden <dless27@gmail.com>
2020-10-13 22:16:42 -0500

    Update the tests with a allow_any_instance_of to simulate a logged in user

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "As an authenticated user" do
+  before :each do
+    @user = User.create!(
+                    username: 'USERNAME',
+                    email: 'example@email.com',
+                    password: 'Hunter2',
+                    role: 0
+                    )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
   describe "When I visit the '/discover' path" do
     it "should have a button to discover top 40 movies" do
       visit '/discover'
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Movies Page as an authenticated user" do
+  before :each do
+    @user = User.create!(
+                    username: 'USERNAME',
+                    email: 'example@email.com',
+                    password: 'Hunter2',
+                    role: 0
+                    )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
 
   it "displays 40 movies with their title and vote average", :vcr do
     visit "/movies"
 
     expect(page).to have_content("Top 40 Movies")
     expect(page).to have_button("Search by Movie Title")
-    expect(page).to have_button("Find Top Rated Movies")
-#     expect(page).to have_content("Find a Movie Here")
     expect(page).to have_link("The Godfather")
     expect(page).to have_content("Vote Average:")
   end
