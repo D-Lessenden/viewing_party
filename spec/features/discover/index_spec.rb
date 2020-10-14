e9d196a8d1779d61b11c6721a79eaa7666681928
Daniel Lessenden <dless27@gmail.com>
2020-10-13 23:48:36 -0500

    Create a test for a shows path

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe "As an user" do
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
-  
+
   describe "When I visit the '/discover' path as an authenticated user" do
-    it "should have a button to discover top 40 movies" do
+    it "should have a button to discover top 40 movies", :vcr do
       @user = User.create!(
                       username: 'USERNAME',
                       email: 'example@email.com',
@@ -19,7 +19,24 @@ RSpec.describe "As an user" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit '/discover'
-      expect(page).to have_button("Discover Top 40")
+      expect(page).to have_button("Discover Top 40 Movies")
+      click_on "Discover Top 40 Movies"
+      expect(current_path).to eq('/movies')
+    end
+
+    it "should have a button to discover top 40 Shows", :vcr do
+      @user = User.create!(
+                      username: 'USERNAME',
+                      email: 'example@email.com',
+                      password: 'Hunter2',
+                      role: 0
+                      )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit '/discover'
+      expect(page).to have_button("Discover Top 40 Shows")
+      click_on "Discover Top 40 Shows"
+      expect(current_path).to eq('/shows')
     end
   end
 end
