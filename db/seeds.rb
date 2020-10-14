ab0bdc46649111f87bfd551c6052f52eb1007f62
Judith Pillado <judith.pillado@gmail.com>
2020-10-14 13:03:39 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Party.destroy_all
+User.destroy_all
+
+# Users:
+user1 = User.create!(username: 'Michael Scott', email: 'michaelscarn@email.com', password: 'holly', role: 0)
+
+# Parties:
+party1 = user1.parties.create!(movie_title: "Schindler's List", duration: 197, date: '10/13/2020', start_time: '7:00 PM CT')
+party2 = user1.parties.create!(movie_title: 'Spirited Away', duration: 125, date: '11/11/2020', start_time: '4:00 PM CT')
+party3 = user1.parties.create!(movie_title: 'Forrest Gump', duration: 142, date: '10/18/2020', start_time: '6:00 PM CT')
