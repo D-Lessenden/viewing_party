c58bb4213e6b12ae255b7ba7e377781aa7767fc6
Daniel Lessenden <dless27@gmail.com>
2020-10-13 22:34:48 -0500

    Add a sad path test for unauthenticated user

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Movies Page as an authenticated user" do
-  before :each do
+  it "displays 40 movies with their title and vote average", :vcr do
     @user = User.create!(
                     username: 'USERNAME',
                     email: 'example@email.com',
@@ -10,14 +10,17 @@ RSpec.describe "Movies Page as an authenticated user" do
                     )
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
-
-  it "displays 40 movies with their title and vote average", :vcr do
     visit "/movies"
 
     expect(page).to have_content("Top 40 Movies")
     expect(page).to have_button("Search by Movie Title")
     expect(page).to have_link("The Godfather")
     expect(page).to have_content("Vote Average:")
+  end
+
+  it "if a non-authenticated user visits the page they recieve an error", :vcr do
+    visit "/movies"
+    save_and_open_page
+    expect(page).to have_content("The page you were looking for doesn't exist.")
   end
 end
