9ddc36cda84085d286bbf4063561fa8afff08c68
Daniel Lessenden <dless27@gmail.com>
2020-10-14 00:49:09 -0500

    Add action for a show pageas well as needed API calls

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -10,4 +10,12 @@ class ShowsController < ApplicationController
     @json1 = JSON.parse(top20.body, symbolize_names: true)
     @json2 = JSON.parse(next20.body, symbolize_names: true)
   end
+
+  def show
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['API-Key'] = ENV['MOVIE_API_KEY']
+    end
+    response = conn.get("/3/tv/76479?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US")
+    @json = JSON.parse(response.body, symbolize_names: true)
+  end
 end
