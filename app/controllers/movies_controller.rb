34ee2406ff4af4ce7d2ddac14fc6a320db5c5228
Daniel Lessenden <dless27@gmail.com>
2020-10-13 22:16:12 -0500

    Fix error in the API call

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,8 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.headers['API-Key'] = ENV['MOVIE_API_KEY']
     end
-    top20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1")
-    next20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=2")
+    top20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&page=1")
+    next20 = conn.get("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&page=2")
     @json1 = JSON.parse(top20.body, symbolize_names: true)
     @json2 = JSON.parse(next20.body, symbolize_names: true)
   end
